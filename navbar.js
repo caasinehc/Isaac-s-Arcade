8c9398c6e93f57269bf507d48cc711fd342310aa
Isaac Chen <31968674+caasinehc@users.noreply.github.com>
2018-01-22 12:08:19 -0500

    Finalized navbar

--- navbar.js
+++ navbar.js
@@ -1,67 +1,125 @@
 (function() {
-	let styles = {
-		navbar: (
-			"list-style-type: none;" +
-			"margin: 0;" +
-			"padding: 0;" +
-			"overflow: hidden;" +
-			"background-color: #000000;"
-		),
-		li: (
-			"float: left"
-		),
-		link: (
-			"display: inline-block;" +
-			"color: white;" +
-			"text-align: center;" +
-			"padding: 14px 16px;" +
-			"text-decoration: none;"
-		)
-	}
-	let thisScript = document.currentScript;
-	let navbar = document.createElement("ul");
-	navbar.style = styles.navbar;
 	let navbarLayout = {
 		home: "../Home/Home.html",
 		games: {
 			game1: "javascript: alert('game1 clicked!');",
 			game2: "javascript: alert('game2 clicked!');",
 			game3: "javascript: alert('game3 clicked!');"
+		},
+		random: {
+			random1: "javascript: alert('random1 clicked!');",
+			random2: "javascript: alert('random2 clicked!');",
+			random3: "javascript: alert('random3 clicked!');"
 		}
-	}
+	};
+	let styles = {
+		border: "1px solid #FFFFFF",
+		width: 16,
+		height: 12,
+		background: "#202020",
+		color: "#FFFFFF",
+		bold: true,
+		size: 16,
+		hover: {
+			background: "#000000",
+			color: "#FFFFFF"
+		},
+		active: {
+			background: "#FFFFFF",
+			color: "#000000"
+		},
+		dropdown: {
+			width: 160,
+			align: "center"
+		}
+	};
+	let thisScript = document.currentScript;
+	let navbarDiv = document.createElement("div");
+	navbarDiv.classList.add("navbar");
+	let navbar = document.createElement("ul");
+	let style = document.createElement("style");
+	style.type = "text/css";
+	style.innerHTML = (
+		// NAVBAR
+		".navbar ul {\n" +
+		"	list-style-type: none;\n" + // no bullets
+		"	margin: 0px;\n" +
+		"	padding: 0px;\n" +
+		"	overflow: hidden;\n" +
+		"	background: " + styles.background + ";\n" +
+		"	border: " + styles.border + ";\n" +
+		"}\n" +
+		// NAVBAR BUTTONS
+		".navbar li {\n" +
+		"	float: left;\n" +
+		"}\n" +
+
+		// LINKS
+		".navbar li a {\n" +
+		"	display: inline-block;\n" + // spacing
+		"	color: " + styles.color + ";\n" +
+		"	font-weight: " + (styles.bold ? "bold" : "normal") + ";\n" +
+		"	font-size: " + styles.size + "px;\n" +
+		"	padding: " + styles.height + "px " + styles.width + "px;\n" +
+		"	text-decoration: none;\n" + // removes underline
+		"}\n" +
+		// LINKS ON HOVER, DROPDOWN BUTTON WHILE IN DROPDOWN
+		".navbar li a:hover, .navbar li:hover .dropdownLink {\n" +
+		"	background: " + styles.hover.background + ";\n" +
+		"	color: " + styles.hover.color + ";\n" +
+		"}\n" +
+
+		// LINKS WHEN ACTIVE, DROPDOWN BUTTON WHEN DROPDOWN ACTIVE
+		".navbar li a.active, .navbar li.active .dropdownLink {\n" +
+		"	background: " + styles.active.background + ";\n" +
+		"	color: " + styles.active.color + ";\n" +
+		"}\n" +
+
+		// DROPDOWN
+		".navbar li div {\n" +
+		"	display: none;\n" + // Hidden by default
+		"	position: absolute;\n" + // positions correctly
+		"	background: " + styles.background + ";\n" +
+		"	border: " + styles.border + ";\n" +
+		"	border-top: none;\n" + // removes top part of border
+		"	min-width: " + styles.dropdown.width + "px;\n" +
+		"	z-index: 1;\n" + // keeps dropdown above everything else
+		"}\n" +
+		// DROPDOWN ON DROPDOWN BUTTON HOVER
+		".navbar li:hover div {\n" +
+		"	display: block;\n" + // show yourself!
+		"}\n" +
+
+		// DROPDOWN LINKS
+		".navbar li div a {\n" +
+		"	display: block;\n" + // stacks links vertically
+		"	text-align: " + styles.dropdown.align + ";\n" +
+		"}"
+	)
 	for(let key of Object.keys(navbarLayout)) {
+		let list = document.createElement("li");
+		let link = document.createElement("a");
+		link.innerText = key;
+		list.appendChild(link);
 		if(typeof navbarLayout[key] === "string") {
-			let list = document.createElement("li");
-			list.style = styles.list;
-			let link = document.createElement("a");
-			link.style = styles.link;
 			link.href = navbarLayout[key];
-			link.innerText = key;
-			list.appendChild(link);
-			navbar.appendChild(list);
 		}
 		else {
-			let list = document.createElement("li");
-			list.style = styles.list;
-			list.style = styles.link;
-			// Dropdown button
-			let link = document.createElement("a");
-			link.style = styles.link;
 			link.href = "javascript: void(0);";
 			link.classList.add("dropdownLink");
-			list.appendChild(link);
 			// Dropdown content
 			let contentDiv = document.createElement("div");
-			contentDiv.classList.add("dropdownContent");
 			for(let key2 of Object.keys(navbarLayout[key])) {
-				let link = document.createElement("a");
-				link.href = navbarLayout[key][key2];
-				link.innerText = key2;
-				contentDiv.appendChild(link);
+				let link2 = document.createElement("a");
+				link2.href = navbarLayout[key][key2];
+				link2.innerText = key2;
+				contentDiv.appendChild(link2);
 			}
 			list.appendChild(contentDiv);
-			navbar.appendChild(list);
 		}
+		navbar.appendChild(list);
 	}
-	thisScript.parentElement.replaceChild(navbar, thisScript);
+	navbarDiv.appendChild(style);
+	navbarDiv.appendChild(navbar);
+	thisScript.parentElement.replaceChild(navbarDiv, thisScript);
 })();
