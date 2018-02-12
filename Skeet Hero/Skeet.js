c05fe2b437f67f23f944645d92d4661c3ff270a6
Isaac Chen <31968674+caasinehc@users.noreply.github.com>
2018-02-12 14:51:08 -0500

    More work on Skeet Hero

--- Skeet Hero/Skeet.js
+++ Skeet Hero/Skeet.js
@@ -1,28 +1,42 @@
 function Skeet() {
-	let gravity = new physics.Vector(0, 0.0057142857142857 * 2); // 2 1 0.5 0.25 0.125 0.0625 0.03125, 0.015625
-
 	this.pos = new physics.Vector(math.coinFlip() ? 0 : width, math.random(midHeight, height));
-	this.initPos = this.pos.clone();
-	this.target = this.pos.clone().addX(700).modX(1400);
-	this.vel = new physics.Vector(this.pos.x === 0 ? 2 : -2, -2);
-	this.rad = 15
+	this.vel = new physics.Vector(this.pos.x === 0 ? 3 : -3, -3);
+	this.acc = new physics.Vector(0, abs(this.vel.x) * this.vel.y * -2 / width);
+	this.rad = 15;
+	this.offTheMap = false;
 	this.dead = false;
+
+	this.lines = function() {
+		let clone = new Skeet();
+		clone.pos = this.pos.clone();
+		clone.vel = this.vel.clone();
+		clone.acc = this.acc.clone();
+
+		stroke(255, 64, 0, 1);
+		line(clone.pos, clone.pos.clone().add(clone.vel.clone().mult(25)));
+
+		let pointList = [clone.pos.clone()];
+		for(let i = 0; i < width; i++) {
+			clone.tick();
+			if(clone.dead) break;
+			pointList.push(clone.pos.clone());
+		}
+
+		stroke(255, 64, 0, 0.5);
+		lines(pointList);
+	}
 
 	this.tick = function() {
 		this.pos.add(this.vel);
-		this.vel.add(gravity);
-
-		if(physics.pointInRect(this.pos, physics.origin(), size)) this.dead = true;
+		this.vel.add(this.acc);
+		if(cheats.magneto && this.pos.distSq(mousePos) > 10) this.vel.towards(mousePos.clone().subtract(this.pos), 20 / this.pos.distSq(mousePos));
+		if(this.pos.x < 0 || this.pos.x > width || this.pos.y < 0 || this.pos.y > height) this.offTheMap = this.dead = true;
 	}
 	this.render = function() {
 		fill("#FF4000");
 		noStroke();
 		point(this.pos, this.rad);
 
-		stroke(255, 64, 0, 0.5);
-		line(this.pos, this.target);
-
-		stroke(colors.RED);
-		line(this.initPos, this.target);
+		if(cheats.lines) this.lines();
 	}
 }
--- Skeet Hero/code.js
+++ Skeet Hero/code.js
@@ -2,6 +2,9 @@ canvas.style.cursor = "none";
 
 let skeets = [];
 let cooldown = 0;
+let score = 0;
+let lives = 5;
+let alive = true;
 let halloweenMode = false;
 let sounds = {
 	gunshot: "Assets/Audio/gunshot.mp3",
@@ -10,6 +13,24 @@ let sounds = {
 let timing = {
 	cooldown: floor(frameRate()),
 	reload: floor(frameRate() * 0.25)
+}
+let cheats = {
+	invul: false,
+	lines: false,
+	iNeverMiss: false,
+	machineGun: false,
+	magneto: false
+}
+
+function startGame() {
+	skeets = [];
+	cooldown = 0;
+	lives = 5;
+	alive = true;
+}
+
+function endGame() {
+	alive = false;
 }
 
 function renderCursor() {
@@ -26,28 +47,50 @@ function renderCursor() {
 
 function shoot() {
 	audio.play(sounds.gunshot);
+	let hit = 0;
 	for(let skeet of skeets) {
-		if(physics.pointInCircle(mousePos, skeet.pos, skeet.rad)) skeet.dead = true;
+		if(physics.pointInCircle(mousePos, skeet.pos, skeet.rad)) {
+			skeet.dead = true;
+			hit++;
+		}
 	}
+	if(hit > 0) score += 5 ** (hit - 1);
 }
 
 function click(e, button) {
-	if(cooldown <= 0) {
+	if(cooldown <= 0 || cheats.machineGun) {
 		cooldown = timing.cooldown;
 		shoot();
 	}
 }
 
 function tick() {
-	if(math.chance(1 / 100)) skeets.push(new Skeet());
+	if(!alive) return;
+	if(lives <= 0) {
+		endGame();
+		return;
+	}
 
 	if(cooldown > 0) cooldown--;
 	if(cooldown === timing.reload) audio.play(sounds.reload);
 
+	if(skeets.length < 30 && math.chance(1 / 100)) skeets.push(new Skeet());
+
 	for(let i = skeets.length - 1; i >= 0; i--) {
 		let skeet = skeets[i];
 		skeet.tick();
+		if(skeet.offTheMap && !cheats.invul) {
+			lives--;
+			skeets = [];
+			break;
+		}
 		if(skeet.dead) skeets.splice(i, 1);
+	}
+
+	if(cheats.iNeverMiss && skeets.length > 0) {
+		mousePos = skeets[0].pos.clone();
+		mouseX = mousePos.x;
+		mouseY = mousePos.y;
 	}
 }
 
@@ -59,10 +102,27 @@ function render() {
 
 	for(let skeet of skeets) skeet.render();
 
+	// recolor(colors.CERULEAN);
+
+	fill(colors.RED);
+	noStroke();
+	rect(width - 150 - 10, height - 20 - 10, lives * 30, 20);
+
+	noFill();
+	stroke(0);
+	lineWidth(2);
+	rect(width - 150 - 10, height - 20 - 10, 150, 20);
+
+	fill(0);
+	noStroke();
+	textAlign("right");
+	text(`Life: `, width - 150 - 10, height - 10);
+
 	renderCursor();
 
 	fill(0);
 	noStroke();
-	textAlign("bottom", "left");
-	text(cooldown, 0, height);
+	font("Arial", 36);
+	textAlign("alphabetic", "left");
+	text(`Score: ${score}`, 10, height - 10);
 }
