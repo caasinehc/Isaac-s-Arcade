b33a05dfe8770d6d500c06d923e60e2ae5511c99
Isaac Chen <31968674+caasinehc@users.noreply.github.com>
2018-02-12 11:37:14 -0500

    have to go downstairs...
    damn it school.....

--- Skeet Hero/Skeet.js
+++ Skeet Hero/Skeet.js
@@ -1,17 +1,28 @@
 function Skeet() {
-	let gravity = new physics.Vector(0, 0.0125);
+	let gravity = new physics.Vector(0, 0.0057142857142857 * 2); // 2 1 0.5 0.25 0.125 0.0625 0.03125, 0.015625
 
 	this.pos = new physics.Vector(math.coinFlip() ? 0 : width, math.random(midHeight, height));
+	this.initPos = this.pos.clone();
+	this.target = this.pos.clone().addX(700).modX(1400);
 	this.vel = new physics.Vector(this.pos.x === 0 ? 2 : -2, -2);
+	this.rad = 15
 	this.dead = false;
 
 	this.tick = function() {
 		this.pos.add(this.vel);
 		this.vel.add(gravity);
+
+		if(physics.pointInRect(this.pos, physics.origin(), size)) this.dead = true;
 	}
 	this.render = function() {
 		fill("#FF4000");
 		noStroke();
-		point(this.pos, 15);
+		point(this.pos, this.rad);
+
+		stroke(255, 64, 0, 0.5);
+		line(this.pos, this.target);
+
+		stroke(colors.RED);
+		line(this.initPos, this.target);
 	}
 }
--- Skeet Hero/code.js
+++ Skeet Hero/code.js
@@ -7,16 +7,48 @@ let sounds = {
 	gunshot: "Assets/Audio/gunshot.mp3",
 	reload: "Assets/Audio/reload.mp3"
 }
+let timing = {
+	cooldown: floor(frameRate()),
+	reload: floor(frameRate() * 0.25)
+}
+
+function renderCursor() {
+	fill(cooldown > timing.reload ? colors.RED : colors.BLACK);
+	noStroke();
+	rect(mouseX - 11, mouseY - 1, 22, 2);
+	rect(mouseX - 1, mouseY - 11, 2, 22);
+
+	stroke(fill());
+	noFill();
+	lineWidth(2);
+	point(mousePos, 11);
+}
+
+function shoot() {
+	audio.play(sounds.gunshot);
+	for(let skeet of skeets) {
+		if(physics.pointInCircle(mousePos, skeet.pos, skeet.rad)) skeet.dead = true;
+	}
+}
 
 function click(e, button) {
-	if(cooldown === 0) {
-		cooldown = floor(1000 / frameRate());
-		setTimeout(() => {audio.play(sounds.reload);}, 750);
+	if(cooldown <= 0) {
+		cooldown = timing.cooldown;
+		shoot();
 	}
 }
 
 function tick() {
+	if(math.chance(1 / 100)) skeets.push(new Skeet());
+
 	if(cooldown > 0) cooldown--;
+	if(cooldown === timing.reload) audio.play(sounds.reload);
+
+	for(let i = skeets.length - 1; i >= 0; i--) {
+		let skeet = skeets[i];
+		skeet.tick();
+		if(skeet.dead) skeets.splice(i, 1);
+	}
 }
 
 function render() {
@@ -24,4 +56,13 @@ function render() {
 	if(window.location.href.startsWith("file:///")) taint();
 	// </TEMP>
 	image("Assets/Images/background.png");
+
+	for(let skeet of skeets) skeet.render();
+
+	renderCursor();
+
+	fill(0);
+	noStroke();
+	textAlign("bottom", "left");
+	text(cooldown, 0, height);
 }
